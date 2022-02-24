7d78e2e668b0f5fe23ec515f5b5cf0d230e73c98
Vishaal Pal <vishaal.pal@contino.io@Vishaals-MacBook-Pro.local>
2022-02-24 16:50:29 +1100

    Updated files for the getting started section

--- meta.tf
+++ meta.tf
@@ -1,8 +1,14 @@
 terraform {
   required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3"
+    }
+  }
   backend "s3" {
     bucket = "pathways-dojo"
-    key    = "<username>-tfstate-main"
+    key    = "vishaalpal-tfstate-main"
     region = "us-east-1"
   }
 }
