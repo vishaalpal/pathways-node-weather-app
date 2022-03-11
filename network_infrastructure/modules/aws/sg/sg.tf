2646c59386499d9d352bbb0d92fb77a5aa7f0a90
Vishaal Pal <vishaal.pal@contino.io>
2022-03-11 14:04:39 +1100

    reversing changes to sg cidr block

--- network_infrastructure/modules/aws/sg/sg.tf
+++ network_infrastructure/modules/aws/sg/sg.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "cw_endpoint_sg" {
       from_port   = ingress.value.from
       to_port     = ingress.value.to
       protocol    = ingress.value.protocol
-      cidr_blocks = var.get_vpc_cidr_block
+      cidr_blocks = ingress.value.cidr
     }
   }
 
@@ -34,7 +34,7 @@ resource "aws_security_group" "cw_endpoint_sg" {
       from_port   = egress.value.from
       to_port     = egress.value.to
       protocol    = egress.value.protocol
-      cidr_blocks = var.get_vpc_cidr_block
+      cidr_blocks = egress.value.cidr
     }
   }
   tags = {
